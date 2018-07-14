d7b69a8ae936c7c9446de7b6c938c1abda49930a
crosenblatt <rosench13@gmail.com>
2018-07-14 13:16:08 -0400

    some refactoring and easter eggs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import json
 import random
+import requests
+import simplejson
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -12,16 +14,33 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
 	data = request.get_json()
-	if data['text'] == 'Hi BeefBuddy':
-		msg = 'Hi {}!'.format(data['name'])
-		send_message(msg)
-	elif data['text'] == 'BeefBuddy, flip a coin':
+	query = data['text'].lower()
+
+	if query == 'hi beefbuddy':
+		sayHi(data['name'])
+
+	if query == 'beefbuddy, list all commands':
+
+
+	if 'beefbuddy, flip a coin' in query:
 		flipACoin()
-	elif 'BeefBuddy, pick a number' in data['text']:
+
+	if 'beefbuddy, pick a number' in query:
 		nums = [int(s) for s in data['text'].split() if s.isdigit()]
-		print(nums)
 		if len(nums) == 2:
 			pickANumber(nums[0], nums[1])
+
+	if 'beefbuddy' in query and 'yes or no' in query:
+		if 'is a hot dog a sandwich' in query:
+			send_message('yes')
+		else:
+			yesOrNo()
+
+	if query == 'where is the center of the maze':
+		maze()
+
+	if query == 'where is the door':
+		door(data['name'])
 
 	return 'OK', 200
 
@@ -36,11 +55,23 @@ def send_message(msg):
 	request = Request(url, urlencode(data).encode())
 	json = urlopen(request).read().decode()
 
+def sayHi(name):
+	send_message('Hi {}!'.format(name))
+
+def listAllCommands():
+	send_message('flip a coin, pick a number, yes or no')
+
 def flipACoin():
-	coin = random.choice([True, False])
-	msg = 'heads' if coin else 'tails'
-	send_message(msg)
+	send_message(random.choice['heads', 'tails'])
 
 def pickANumber(start, end):
-	rand = random.randint(start, end)
-	send_message(str(rand))
+	send_message(str(random.randint(start, end)))
+
+def yesOrNo():
+	send_message(random.choice(['yes', 'no']))
+
+def maze():
+	send_message('the maze wasn\'t meant for you')
+
+def door(name):
+	send_message('this game was meant for you, {}, but you must play it alone'.format(name))
